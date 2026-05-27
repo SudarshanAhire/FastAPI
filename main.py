a2f5e498bf67cad82c02b8334d50c5f4bdf24aaa
SudarshanAhire <sudarshanahire292@gmail.com>
2026-05-27 17:14:16 +0530

    added sort get request

--- main.py
+++ main.py
@@ -31,3 +31,22 @@ def view_patients(patient_id: str = Path(..., description="ID of the patient in 
     if patient_id in data:
         return data[patient_id]
     raise HTTPException(status_code=404, detail="Patient not found")
+
+@app.get("/sort")
+def sort_patients(sort_by: str = Query(..., description="Sort on the basis of height, weight or bmi"), order: str = Query("asc", description="Sort in asc or desc order")):
+    
+    valid_feilds = ["height", "weight", "bmi"]
+
+    if sort_by not in valid_feilds:
+        raise HTTPException(status_code=400, detail=f"Invalid feild select from {valid_feilds}")
+    
+    if order not in ['asc', 'desc']:
+        raise HTTPException(staus_code=400, detail="Invalid order select between asc and desc")
+    
+    data = load_data()
+
+    sort_order = True if order == "desc" else False 
+
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
+
+    return sorted_data    
